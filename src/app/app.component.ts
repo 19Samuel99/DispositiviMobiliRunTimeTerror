5ce9fdf4a1191901461714513f70be5e10d4a13a
solazzol <solazzolorenzo@libero.it>
2021-06-02 12:47:42 +0200

    Aggiunta file constants.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() {}
+
+  initTranslate() {
+    // Set the default language for translation strings, and the current language.
+    const linguaPreferita = this.linguaService.getLinguaPreferita();
+    this.translate.setDefaultLang(linguaPreferita);
+    this.linguaService.getLinguaAttuale().subscribe((lingua: string) => {
+      if (lingua != null) {
+        this.translate.use(lingua);
+      } else {
+        this.translate.use(linguaPreferita);
+        this.linguaService.updateLingua(linguaPreferita);
+      }
+    });
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {IonicStorageModule} from '@ionic/storage-angular';
+import {httpInterceptorProviders} from './interceptors';
 
-import {IonicStorageModule} from '@ionic/storage';
+// The translate loader needs to know where to load i18n files
+// in Ionic's static asset pipeline.
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot(), TranslateModule.forRoot({
+      loader: {provide: TranslateLoader,useFactory: (createTranslateLoader),deps: [HttpClient]}})],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
