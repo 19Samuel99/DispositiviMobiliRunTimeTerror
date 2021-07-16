67eb5b65daa5ff28506fadd8a195e0d1e0d405b9
AndreaLarivera <64405125+AndreaLarivera@users.noreply.github.com>
2021-07-16 13:19:44 +0200

    scheda informativa

--- MoviesToWatch/src/app/pages/schedainformativa/schedainformativa.page.ts
+++ MoviesToWatch/src/app/pages/schedainformativa/schedainformativa.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-schedainformativa',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SchedainformativaPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+  linkHome(){
+    this.router.navigate(['/home']);
+  }
 
+  linkFilmvisti(){
+    this.router.navigate(['/giavisti']);
+  }
+
+  linkFilmdavedere(){
+    this.router.navigate(['/davedere']);
+  }
+
+  linkAreautente(){
+    this.router.navigate(['/areautente']);
+  }
+
+  linkRicerca(){
+    this.router.navigate(['/ricerca']);
+  }
 }
