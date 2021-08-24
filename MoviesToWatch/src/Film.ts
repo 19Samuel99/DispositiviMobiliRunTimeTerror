99c791134b20b07328a4cca9be4997c74444b351
samuel <samcip99@gmail.com>
2021-08-24 20:02:21 +0200

    committo ricerca page con una fetch giusta, bisogna vedere se viene richiamata la funzione perchè non mi sembra ma è tardi vado a mangiare

---
+++ MoviesToWatch/src/Film.ts
@@ -0,0 +1,13 @@
+export class Film {
+    id: string;
+    imageUrl: URL;
+    attori: string[];
+    personaggi: string[];
+    durata: number;
+    titolo: string;
+    tipo: string;
+    genere: string[];
+    anno: number;
+
+
+}
--- MoviesToWatch/src/app/app.component.ts
+++ MoviesToWatch/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TestService} from "./services/test.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private heroesUrl: TestService
+
+  ) {}
 }
