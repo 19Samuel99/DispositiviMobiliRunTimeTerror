8e87db18a2cfc8a9036fbf7ee1f1b2310474dcfd
solazzol <solazzolorenzo@libero.it>
2021-09-06 17:22:32 +0200

    piccolo fix

--- MoviesToWatch/src/app/pages/areautente/areautente.page.ts
+++ MoviesToWatch/src/app/pages/areautente/areautente.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import firebase from 'firebase';
-import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-areautente',
