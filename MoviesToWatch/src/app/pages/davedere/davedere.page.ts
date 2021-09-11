bfeddb01c110ebf40f6f22f7e08e306f10aec3b4
samuel <samcip99@gmail.com>
2021-09-11 16:22:03 +0200

    ora vediamo gli array davedere e giavisti

--- MoviesToWatch/src/app/pages/davedere/davedere.page.ts
+++ MoviesToWatch/src/app/pages/davedere/davedere.page.ts
@@ -11,7 +11,7 @@ import {AngularFireAuth} from '@angular/fire/auth';
   styleUrls: ['./davedere.page.scss'],
 })
 export class DavederePage implements OnInit {
- arrayDaVedere = []
+
   constructor(private router: Router, private activatedRoute: ActivatedRoute , private firestore: AngularFirestore,
               public auth: AngularFireAuth) { }
 
@@ -41,9 +41,29 @@ export class DavederePage implements OnInit {
     this.router.navigate(['/schedainformativa']);
   }
    async controllaDaVedere() {
+let arraiDiID = []
+     const promise = this.firestore.collection('Utenti').doc(firebase.auth().currentUser.uid).get()
+     promise.toPromise().then(snapshot =>{
+       const data = snapshot.data()
+       console.log(data)
+       for(let i = 0; i< data['davedere'].length; i++){
+         arraiDiID[i] = data['davedere'][i]
+       console.log(arraiDiID[i])
+         console.log(arraiDiID)
+     }
+       console.log('questo è array fuori dal ciclo', arraiDiID)
 
- return await this.firestore.collection('Utenti').doc(firebase.auth().currentUser.uid).get().subscribe(snapshot=> console.log('document data:' , snapshot.data()))
-    //return await this.firestore.collection('Utenti').doc(firebase.auth().currentUser.uid).get().subscribe(snapshot=> console.log('document data:' , snapshot.data().davedere))
-    console.log('ma funziona?')
-  }
+     })
+
+       }
+
 }
+  //return await this.firestore.collection('Utenti').doc(firebase.auth().currentUser.uid).get().subscribe(snapshot=> console.log('document data:' , snapshot.data()))
+    //return await this.firestore.collection('Utenti').doc(firebase.auth().currentUser.uid).get().subscribe(snapshot=> console.log('document data:' , snapshot.data().davedere))
+   // console.log('ma funziona?')
+ //
+
+/* let   rootRef = firebase.database().ref();
+   let  currentUID = firebase.auth().currentUser.uid
+   let  oneRef = rootRef.child('Utenti').child(currentUID).child('davedere')
+console.log(oneRef)*/
--- MoviesToWatch/src/app/pages/giavisti/giavisti.page.ts
+++ MoviesToWatch/src/app/pages/giavisti/giavisti.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
-
+import { ActivatedRoute } from '@angular/router';
+import firebase from 'firebase/app';
+import 'firebase/app';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {AngularFireAuth} from '@angular/fire/auth';
 @Component({
   selector: 'app-giavisti',
   templateUrl: './giavisti.page.html',
@@ -8,9 +12,11 @@ import {Router} from '@angular/router';
 })
 export class GiavistiPage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private activatedRoute: ActivatedRoute , private firestore: AngularFirestore,
+              public auth: AngularFireAuth) { }
 
   ngOnInit() {
+    this.controllaGiaVisti()
   }
   linkHome(){
     this.router.navigate(['/home']);
@@ -33,5 +39,21 @@ export class GiavistiPage implements OnInit {
   }
   linkSchedainformativa(){
     this.router.navigate(['/schedainformativa']);
+  }
+  async controllaGiaVisti() {
+    let arraiDiID = []
+    const promise = this.firestore.collection('Utenti').doc(firebase.auth().currentUser.uid).get()
+    promise.toPromise().then(snapshot =>{
+      const data = snapshot.data()
+      console.log(data)
+      for(let i = 0; i< data['giavisti'].length; i++){
+        arraiDiID[i] = data['giavisti'][i]
+        console.log(arraiDiID[i])
+        console.log(arraiDiID)
+      }
+      console.log('questo è array fuori dal ciclo', arraiDiID)
+
+    })
+
   }
 }
